7aab4145f423396235844e420539275122e62b64
Norman Cook <issaih.mendiola@gmail.com>
2025-08-13 16:33:29 +0800

    fetching

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,7 @@
 import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
+import { client } from "@/sanity/lib/client";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({
   searchParams,
@@ -8,21 +10,7 @@ export default async function Home({
 }) {
   const query = (await searchParams).query;
 
-  const posts = [
-    {
-      _createdAt: new Date(),
-      views: 55,
-      author: { 
-        _id: 1,
-        name: "Norman Cook"
-      },
-      id: 1,
-      description: 'This is a description',
-      image: 'https://robohash.org/YOUR-TEXT.png',
-      category: "Robots",
-      title: "We Robots"
-    },
-  ];
+  const posts = await client.fetch(STARTUPS_QUERY)
 
   return (
     <>
