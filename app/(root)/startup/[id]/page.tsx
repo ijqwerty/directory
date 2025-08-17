f9bf41d0f23ffc570ca73331a97fac35d0a7b4a4
Issaih Jeremiah Mendiola <issaih.mendiola@gmail.com>
2025-08-17 23:26:53 +0800

    3:09:23

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { formatDate } from "@/lib/utils";
+import { client } from "@/sanity/lib/client";
+import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
+import { notFound } from "next/navigation";
+import React from "react";
 
-const page = () => {
+export const experimental_ppr = true;
+
+const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const id = (await params).id;
+  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+
+  if (!post) return notFound()
+
   return (
-    <div>page</div> // continue here
-  )
-}
+    <>
+      <section className="pink_container !min-h-[230px]">
+        <p className="tag">
+          {formatDate(post?._createdAt)}
+        </p>
+      </section>
+    </>
+  );
+};
 
-export default page
+export default Page;
